be6b3b3bb616c395d53ac3ca9b44fa382e8efd9e
inimitableharish <inimitableharish@gmail.com>
2016-07-03 02:45:30 +0530

    models modified

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'settings',
+    'videos',
 ]
 
 MIDDLEWARE_CLASSES = [
--- project/videos/models.py
+++ project/videos/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Videos(models.Model):
+    url = models.CharField(max_length=255)
+
