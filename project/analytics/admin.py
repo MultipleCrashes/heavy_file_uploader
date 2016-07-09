c87b785e690d3592e7752b592660860aa3d9d782
inimitableharish <inimitableharish@gmail.com>
2016-07-09 23:21:54 +0530

    admin fields added

--- project/analytics/admin.py
+++ project/analytics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class AnalyticsAdmin(admin.ModelAdmin):
+    pass 
+
+admin.site.register(AnalyticsSource,AnalyticsAdmin)
