0a43018653c1f2c8704c8ddebb423e2f1eb4e95f
inimitableharish <inimitableharish@gmail.com>
2016-07-09 23:16:28 +0530

    minor changes analytics app

--- project/analytics/models.py
+++ project/analytics/models.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
 # Create your models here.
+
+class AnalyticsSource(models.Model):
+    file_url = models.CharField(max_length=255)
+    file_name = models.CharField(max_length=255)
+    file_type = models.CharField(max_length=255)
