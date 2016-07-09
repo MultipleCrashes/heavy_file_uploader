6b3e4ce6245bf45e82971337d67b0d09b61c52c4
inimitableharish <inimitableharish@gmail.com>
2016-07-09 19:19:18 +0530

    project/settings.py
    auth user model ,custom user

--- project/project/settings.py
+++ project/project/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Custom entries harish 
+AUTH_USER_MODEL = 'register_user.User'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
