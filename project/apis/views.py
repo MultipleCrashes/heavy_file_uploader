297c9de199d53a146a438fd27a048cfa0a8d2a2e
inimitableharish <inimitableharish@gmail.com>
2016-07-09 21:08:27 +0530

    views modified

--- project/apis/views.py
+++ project/apis/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import *
+from django.views.generic import View 
 # Create your views here.
+
+class ApiStructure(View):
+    def __init__(self):
+        pass 
+
+    def dispatch(self, request):
+        return super(ApiStructure, self).dispatch(*args, **kwars) 
+
+    def post(self, request):
+        req_data = request.POST
+        pass 
+
+    def get(self, request):
+        req_data = request.GET 
+        pass 
