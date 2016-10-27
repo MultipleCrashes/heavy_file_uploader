78f858a6ea43ae964e21d1f68fdb072c1524b9f5
harish upadhyay <inimitableharish@gmail.com>
2016-10-27 19:40:28 +0000

    updated few lines

--- project/auth_layer/auth_module.py
+++ project/auth_layer/auth_module.py
@@ -8,7 +8,7 @@ class AuthClass:
     def hash_password(password, maxtime=0.5, datalength=64):
         return scrypt.encrypt(randstr(datalength), password, maxtime=maxtime)
 
-    def verify_password(saved_password_hash, entered_password_hash, maxtime=0.5)
+    def verify_password(saved_password_hash, entered_password_hash, maxtime=0.5):
         try:
             scrypt.decrypt(saved_password_hash, entered_password_hash, maxtime)
             retun True 
@@ -16,7 +16,11 @@ class AuthClass:
             return False 
 
 
-if __name__=='__main__':
+if __name__== '__main__':
     user_pass = 'harishpass'
-    user_salt = randstr(2)
-    pw_salt = user
+    user_salt = randstr(5)  # also store salt as plain text
+    pw_salt = user_pass + user_salt 
+    hashed_pw = hash_password(pw_salt)
+    # get plain salt from db and password entered by the user ,
+    # hash the combination and match with what is stored in the db 
+    verify_password(hash_password,pw_salt)   # returns True
