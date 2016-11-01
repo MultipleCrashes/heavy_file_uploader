f77145575643d127160d4c584f1590f556b6b4fb
inimitableharish <inimitableharish@gmail.com>
2016-11-01 18:51:01 +0530

    stripe_utils.py
    token and charge

--- project/paymeant/stripe_utils.py
+++ project/paymeant/stripe_utils.py
@@ -1,23 +1,37 @@
-stripe_secret_key = 'sk_test_YWpFhaSbJp7nolsdKx5iHY9r'
+stripe_secret_key = 'sk_test_BQokikJOvBiI2HlWgH4olfQ2'
 import request
+import stripe 
 
 class StripeClass:
 	
-	def __init__(self):
-		pass
+    def __init__(self):
+        self.transaction_token = ''
+        pass
 
-	def charge_card(self):
+    ''' Steps 
+        1. Generate Token
+        2. Charge Card 
+    '''
+    
+    def generate_token(self,):
+        self.transction_token = stripe.Token.create(
+                card={"number":"4242424242424242",
+                      "exp_month":12,
+                      "exp_year":2016,
+                      "cvc":"123"},
+                )
 
-		token = request.POST['stripeToken']
-		try:
-			charge = stripe.Charge.create(
-				amount =1,
-				currency='INR',
-				source=token,
-				description='Test Charge'
-				)
-		except stripe.error.CardError as e:
-			pass 
+    def charge_card(self):
+        token = self.transaction_token
+	try:
+	    charge = stripe.Charge.create(
+		        amount =1,
+			currency='INR',
+			source=token,
+			description='Test Charge'
+		    )
+	    except stripe.error.CardError as e:
+		print str(e) 
 
 
 if __name__=='__main__':
