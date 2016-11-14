1da68a71bb7a446320e0b57a4d5e7bef9ead447d
harish_upadhyay <inimitableharish@gmail.com>
2016-11-14 16:18:32 +0530

    redis function added

--- pushElementToQueue.js
+++ pushElementToQueue.js
@@ -1,4 +1,18 @@
+var redis = require('redis');
 
+
+var redisConnect = function(){
+
+        var client = redis.createClient(6379,'127.0.0.1');
+	
+	client.on('connection',function(){
+		console.log('redis connected');
+	});
+
+		client.set('taskharish','taskid123');
+		client.set('task2','harish2');
+		client.quit();
+}
 
 var pushToQueue = function(elementToPush){
 	var queue = [];
@@ -6,6 +20,8 @@ var pushToQueue = function(elementToPush){
 	console.log("Elements in the queue are "+ elementToPush);
      }
 
+redisConnect();
 pushToQueue("harish");
 pushToQueue(123);
 module.exports.pushToQueue = pushToQueue;
+module.exports.redisConnect = redisConnect;
