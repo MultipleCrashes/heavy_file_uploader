943270ae0dba7764121f2c79ee2fdb558a330a20
harish_upadhyay <harish.u@happay.in>
2016-11-20 23:23:02 +0530

    loop added

--- http_node_request.js
+++ http_node_request.js
@@ -1,6 +1,12 @@
 var request = require('request');
-requestUrl = request('http://google.com',function(error , response, body){
+var sleep = require('sleep');
+
+for (var i =0; i < 500;i++){
+	console.log("Request no "+i);
+	requestUrl = request('http://localhost:3000',function(error , response, body){
 		if(!error && response.statusCode ==200){
-			console.log(body)
+			console.log(body);
 			}
-		})
+	})
+
+}
