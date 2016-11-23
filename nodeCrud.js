4e19cbe21d5fcf84f001237973ce641fd0df9599
harish_upadhyay <harish.u@happay.in>
2016-11-24 04:07:31 +0530

    insert fn added

--- nodeCrud.js
+++ nodeCrud.js
@@ -1,8 +1,28 @@
 var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = "mongodb://localhost:27017/harish";
+
+var dbName = "proddb";
+
+console.log("Connecting to mongodb at ->" + url);
+
+exports.Connect = MongoClient.connect(url, function(err,db){
+   if(!err){
+   console.log("Sucessfully connected to database");
+   var collection = db.collection(dbName);
+   console.log(collection.insertOne({"xyz":123}));
+   //allDocuments = collection.find({});
+//console.log(allDocuments);
+   db.close();    // Do not forget to close the db handle 
+   }
+   else
+	{
+   	console.log("Could not connect to mongodb database");
+	}   
+ });
 
 
-exports.insertOne(db,collection,document,callback){
 
-	var collection = db.collection(collection);
-	collection.insertOne(document);
-});
+
+
--- nodeMongo.js
+++ nodeMongo.js
@@ -1,8 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
-
-var url = "mongodb://localhost:27017/harish"
+var url = "mongodb://localhost:27017/harish";
 
 var dbName = "proddb";
 
@@ -12,6 +11,9 @@ exports.Connect = MongoClient.connect(url, function(err,db){
    if(!err){
    console.log("Sucessfully connected to database");
    var collection = db.collection(dbName);
+   console.log(collection.insertOne({"xyz":123}));
+   //allDocuments = collection.find({});
+//console.log(allDocuments);
    db.close();    // Do not forget to close the db handle 
    }
    else
